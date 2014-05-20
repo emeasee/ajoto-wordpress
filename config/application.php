3806ea4f4081e5d34b22b07eb31751fdf099d652
Mac Oosthuizen <hello@m-jo.co>
2014-05-21 00:42:22 +0200

    Added Dokku / Heroku specific configurations

--- config/application.php
+++ config/application.php
@@ -9,6 +9,7 @@ if (file_exists($root_dir . '/.env')) {
   Dotenv::load($root_dir);
 }
 
+
 Dotenv::required(array('DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL'));
 
 /**
--- config/environments/development.php
+++ config/environments/development.php
@@ -3,11 +3,16 @@
 define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USER'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
+if (getenv('DB_PORT')) {
+  define('DB_HOST', getenv('DB_HOST').':'.getenv('DB_PORT'));
+} else {
+  define('DB_HOST', 'localhost');
+}
 
-define('WP_HOME', getenv('WP_HOME'));
-define('WP_SITEURL', getenv('WP_SITEURL'));
+define('WP_HOME',(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://".$_SERVER['HTTP_HOST'] : "http://".$_SERVER['HTTP_HOST']);
+define('WP_SITEURL', WP_HOME.'/wp');
 
 define('SAVEQUERIES', true);
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
+define('WP_DEBUG_LOG', true);
 define('SCRIPT_DEBUG', true);
